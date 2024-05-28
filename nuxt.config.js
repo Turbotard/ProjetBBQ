88f9523a5ab458fb0803d60b254a8035b327afac
Turbotard <turbotard9@gmail.com>
2024-05-28 15:26:13 +0200

    chore: Enable devtools in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,13 @@
 export default defineNuxtConfig({
   css: ['vuetify/styles'],
+
   build: {
     transpile: ['vuetify'],
   },
+
   plugins: ['~/plugins/vuetify.js'],
+
+  devtools: {
+    enabled: true,
+  },
 });
